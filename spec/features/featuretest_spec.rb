ab0c5cabd80abcfa9bb45240372af0646675cbdf
ALRW <awerner1@googlemail.com>
2015-10-29 15:42:40 +0000

    added attack

--- spec/features/featuretest_spec.rb
+++ spec/features/featuretest_spec.rb
@@ -8,4 +8,8 @@ feature 'Battle!' do
   scenario 'player 1 can see player2\'s hitpoints' do
     expect(page).to have_content("Batman's HP: 100")
   end
+  scenario 'player 1 attacks player 2' do
+    click_button('Attack')
+    expect(page).to have_content('Pikachu does a MASSIVE attack on Batman')
+  end
 end
