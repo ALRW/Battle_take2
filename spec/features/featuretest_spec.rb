e3278e05678ff7949f18db693e7cb19f53a6d8d7
ALRW <awerner1@googlemail.com>
2015-10-29 13:51:46 +0000

    can view Player 2 HP

--- spec/features/featuretest_spec.rb
+++ spec/features/featuretest_spec.rb
@@ -1,9 +1,14 @@
-feature 'Fill in the names' do
-  scenario 'enter player names'do
+feature 'Battle!' do
+  before do
     visit('/')
     fill_in('player1', with: "Pikachu")
     fill_in('player2', with: "Batman")
     click_button('Submit')
+  end
+  scenario 'enter player names'do
     expect(page).to have_content("Pikachu vs Batman")
+  end
+  scenario 'player 1 can see player2\'s hitpoints' do
+    expect(page).to have_content("Batman's HP: 100")    
   end
 end
